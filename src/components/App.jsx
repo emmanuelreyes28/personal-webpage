fb52e88c248133ef4a37dd59c370d65d497aa0ce
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-02-02 23:34:22 -0800

    Inital commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Intro from "./Intro";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Emmanuel Reyes</h1>
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div>
+      <div>
+        <h1>Hello</h1>
+        <p>I am Emmanuel Reyes a Software Engineer. </p>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
