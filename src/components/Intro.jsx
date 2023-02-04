793a1cb335f82f5f3641dd2ab64960b9f53cafa9
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-02-03 23:42:52 -0800

    Added new components and updated intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,16 @@ function Intro() {
     <div>
       <div>
         <h1>Hello</h1>
-        <p>I am Emmanuel Reyes a Software Engineer. </p>
+        <p>
+          Hello and welcome to my corner of the internet! I'm a Software
+          Engineer with a passion for programming and staying up-to-date with
+          the latest technologies. When I'm not coding, I enjoy keeping active
+          through exercise and spending quality time with friends at local
+          breweries, I believe life is all about balance. I'm always seeking
+          opportunities for growth and development as a Software Engineer, and I
+          strive to be part of a company that supports and challenges me in this
+          pursuit.
+        </p>
       </div>
     </div>
   );
