4ee4e025c86f699ecd26b545925b5890710f5e87
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-02-04 00:12:39 -0800

    Update intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,12 +8,27 @@ function Intro() {
         <p>
           Hello and welcome to my corner of the internet! I'm a Software
           Engineer with a passion for programming and staying up-to-date with
-          the latest technologies. When I'm not coding, I enjoy keeping active
-          through exercise and spending quality time with friends at local
-          breweries, I believe life is all about balance. I'm always seeking
-          opportunities for growth and development as a Software Engineer, and I
-          strive to be part of a company that supports and challenges me in this
-          pursuit.
+          the latest technologies. Before starting my career as a Software
+          Engineer, I had the privilege of inspiring the next generation of
+          coders as a computer science instructor. I taught middle and high
+          school students the fundamentals of programming through exciting and
+          interactive projects, and it was an incredibly rewarding experience.
+        </p>
+        <p>
+          I have experience with a variety of technlogies but the most recent
+          stack I have learned is MERN. I learned MERN through the Complete 2022
+          Web Development Bootcamp on Udemy instructed by Angela Yu. It was an
+          awesome experience where I learned new concepts such as security and
+          how to keep users data safe using hashing and salting or even third
+          party logins. My career goal is to become a Full Stack Engineer, and I
+          am always seeking opportunities for growth and development. I strive
+          to be part of a company that supports and challenges me in this
+          pursuit, and I am excited to see what the future holds.
+        </p>
+        <p>
+          When I'm not coding, I enjoy keeping active through exercise and
+          spending quality time with friends at local breweries, I believe life
+          is all about balance.
         </p>
       </div>
     </div>
