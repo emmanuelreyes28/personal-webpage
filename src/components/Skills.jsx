e63c89a55c2a438dbbb7c70daa2385458ccff30e
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-02-12 16:06:10 -0800

    Edit skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,6 +12,36 @@ function Skills() {
       <div className="w-1/2 my-16 mx-auto text-left">
         <img
           className="w-1/4 float-left mr-7"
+          src={whisperImage}
+          alt="woman shh icon"
+        />
+        <h3 className="font-heading font-bold text-lg my-4 text-orange">
+          Whisper Copycat Web App
+        </h3>
+        <p className="font-semibold pb-1">Backend: Express.js | MongoDB</p>
+        <p className="font-semibold pb-1">
+          Frontend: Embedded JavaScript Templates (EJS)
+        </p>
+        <p className="pb-2">
+          The primary focus of this project was centered around the essential
+          topics of authentication and security. I made sure to keep the user's
+          login credentials safe and secure by utilizing the tried-and-true
+          method of hashing and salting in MongoDB.{" "}
+        </p>
+        <p className="pb-2">
+          Furthermore, to enhance user experience and add an extra layer of
+          security, I went ahead and implemented OAuth 2.0 by integrating
+          Passport.js, allowing users to seamlessly log in using their Google
+          accounts.
+        </p>{" "}
+        <p>
+          Finally, I streamlined the user experience by using cookies to help
+          speed up access to the site for previously logged in users.
+        </p>
+      </div>
+      <div className="w-1/2 my-16 mx-auto text-left">
+        <img
+          className="w-1/4 float-left mr-7"
           src={toDoListImage}
           alt="to-do list icon"
         />
@@ -62,6 +92,7 @@ function Skills() {
           object destructuring and event handling.
         </p>
       </div>
+
       <div className="w-1/2 my-16 mx-auto text-left">
         <img
           className="w-1/4 float-left mr-7"
@@ -75,14 +106,17 @@ function Skills() {
         <p className="font-semibold pb-1">
           Frontend: Embedded JavaScript Templates (EJS)
         </p>
+        <p className="pb-2">
+          This blog site was carefully crafted with a combination of express,
+          ejs, bodyParser, mongoose, and lodash. Through this project, I
+          acquired valuable insights into creating routes, sending both get and
+          post requests, and working with local databases through MongoDB.
+        </p>
         <p>
-          This blog site was used using express, ejs, bodyParser, mongoose and
-          lodash. In this project I learned how to create routes, send get
-          requests, and post requests. All of the blog data was stored in a
-          local database using MongoDB and mongoose. Used vanilla javascript
-          within ejs templates to dynamically render every blog post in the home
-          page. Users can go to a specific post by passing in a post id to
-          /posts/:postId route.
+          In particular, I employed vanilla JavaScript within ejs templates to
+          render every blog post dynamically on the home page. Additionally, I
+          made it possible for users to access a specific post by simply passing
+          in a post id to the designated route: /posts/:postId.
         </p>
       </div>
     </div>
