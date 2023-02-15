fbcb9a768e040086ae0c82ed8e4c306c34ad5477
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-02-14 23:09:06 -0800

    Edit intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -25,13 +25,13 @@ function Intro() {
           I have experience with a variety of technlogies but the most recent
           stack I have learned is MERN. I learned MERN through the Complete 2022
           Web Development Bootcamp on Udemy instructed by Angela Yu. It was an
-          awesome experience where I learned new concepts such as security and
-          how to keep users data safe using hashing and salting or even third
-          party logins amongst a variety of topics and technologies. My career
-          goal is to become a Full Stack Engineer, and I am always seeking
-          opportunities for growth and development. I strive to be part of a
-          company that supports and challenges me in this pursuit, and I am
-          excited to see what the future holds.
+          awesome experience where I learned new concepts such as authentication
+          and security to keep users data safe using hashing, salting and OAuth
+          amongst a variety of topics and technologies. My career goal is to
+          become a Full Stack Engineer, and I am always seeking opportunities
+          for growth and development. I strive to be part of a company that
+          supports and challenges me in this pursuit, and I am excited to see
+          what the future holds.
         </p>
         <p className="pb-2">
           When I'm not coding, I enjoy keeping active through exercise and
