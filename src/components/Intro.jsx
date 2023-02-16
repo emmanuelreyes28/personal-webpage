7947386d08d70600d5af13f28ffe01243632941b
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-02-15 22:48:12 -0800

    Edit intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -17,21 +17,21 @@ function Intro() {
           Engineer with a passion for programming and staying up-to-date with
           the latest technologies. Before starting my career as a Software
           Engineer, I had the privilege of inspiring the next generation of
-          coders as a computer science instructor. I taught middle and high
-          school students the fundamentals of programming through exciting and
+          coders as a Programming Instructor. I taught middle and high school
+          students the fundamentals of programming through exciting and
           interactive projects, and it was an incredibly rewarding experience.
         </p>
         <p className="pb-2">
-          I have experience with a variety of technlogies but the most recent
-          stack I have learned is MERN. I learned MERN through the Complete 2022
-          Web Development Bootcamp on Udemy instructed by Angela Yu. It was an
-          awesome experience where I learned new concepts such as authentication
-          and security to keep users data safe using hashing, salting and OAuth
-          amongst a variety of topics and technologies. My career goal is to
-          become a Full Stack Engineer, and I am always seeking opportunities
-          for growth and development. I strive to be part of a company that
-          supports and challenges me in this pursuit, and I am excited to see
-          what the future holds.
+          As a tech enthusiast, I have gained a wealth of experience in various
+          technologies, and I am excited to offer my expertise to your company.
+          I am particularly excited to share my knowledge of MERN, which I
+          learned through the Complete 2022 Web Development Bootcamp on Udemy,
+          instructed by Angela Yu. During this course, I gained hands-on
+          experience in essential concepts such as creating databases, schemas,
+          routing, rest apis, authentication and security among other key topics
+          and technologies. My career goal is to become a Full Stack Engineer,
+          and I am confident that my skills and expertise will contribute
+          significantly to your team.
         </p>
         <p className="pb-2">
           When I'm not coding, I enjoy keeping active through exercise and
